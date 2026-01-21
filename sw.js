d6d89ee2e13243edfcdb7f9eb127833b93ef157e
albaman93-creator <albaman93@gmail.com>
2026-01-22 06:09:49 +0700

    varifikasi timbangan

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'timbangan-app-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
